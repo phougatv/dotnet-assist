ffaea8baa166b1919d509b5aaa6a09c7d499acc5
Vedant Phougat <phougatv@outlook.com>
2022-08-26 12:17:35 +0530

    Added Int16 UTCs

---
+++ test/Assist/UnitTests/NumericExtensionTests/Int16_IsPositiveShould.cs
@@ -0,0 +1,64 @@
+﻿namespace VP.DotNet.Assist.UnitTest.NumericExtensionTests;
+
+using FluentAssertions;
+using System;
+using VP.DotNet.Assist.Extensions;
+using Xunit;
+
+public class Int16_IsPositive
+{
+	[Fact]
+	public void NotThrow_NotImplementedException()
+	{
+		//Arrange
+		var intMaxValue = Int16.MaxValue;
+		var intMinValue = Int16.MinValue;
+
+		//Act
+		Action actWhenInt16Max = () => intMaxValue.IsPositive();
+		Action actWhenInt16Min = () => intMinValue.IsPositive();
+
+		//Assert
+		actWhenInt16Max.Should().NotThrow<NotImplementedException>();
+		actWhenInt16Min.Should().NotThrow<NotImplementedException>();
+	}
+
+	[Fact]
+	public void ReturnFalse_WhenNumberIsZero()
+	{
+		//Arrange
+		var intZero = (Int16)(Int16.MaxValue + Int16.MinValue + 1);
+
+		//Act
+		var actualWhenIntIsZero = intZero.IsPositive();
+
+		//Assert
+		actualWhenIntIsZero.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnFalse_WhenNumberIsNegative()
+	{
+		//Arrange
+		var intNegative = Int16.MinValue;
+
+		//Act
+		var actualWhenIntIsNegative = intNegative.IsPositive();
+
+		//Assert
+		actualWhenIntIsNegative.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnTrue_WhenNumberIsPositive()
+	{
+		//Arrange
+		var intPositive = Int16.MaxValue;
+
+		//Act
+		var actualWhenIntIsPositive = intPositive.IsPositive();
+
+		//Assert
+		actualWhenIntIsPositive.Should().BeTrue();
+	}
+}
---
+++ test/Assist/UnitTests/NumericExtensionTests/Int16_IsZeroShould.cs
@@ -0,0 +1,60 @@
+﻿namespace VP.DotNet.Assist.UnitTest.NumericExtensionTests;
+
+using FluentAssertions;
+using System;
+using VP.DotNet.Assist.Extensions;
+using Xunit;
+
+public class Int16_IsZeroShould
+{
+	[Fact]
+	public void NotThrow_NotImplementedException()
+	{
+		//Arrange
+		var int16MaxValue = Int16.MaxValue;
+		var int16MinValue = Int16.MinValue;
+
+		//Act
+		Action actWhenInt16Max = () => int16MaxValue.IsZero();
+		Action actWhenInt16Min = () => int16MinValue.IsZero();
+
+		//Assert
+		actWhenInt16Max.Should().NotThrow<NotImplementedException>();
+		actWhenInt16Min.Should().NotThrow<NotImplementedException>();
+	}
+
+	[Fact]
+	public void ReturnFalse_WhenNumberIsNegative()
+	{
+		//Act
+		var actualWhenInt16Min = Int16.MinValue.IsZero();
+
+		//Assert
+		Int16.MinValue.Should().BeNegative();
+		actualWhenInt16Min.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnFalse_WhenNumberIsPositive()
+	{
+		//Act
+		var actualWhenInt16Min = Int16.MaxValue.IsZero();
+
+		//Assert
+		Int16.MaxValue.Should().BePositive();
+		actualWhenInt16Min.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnTrue_WhenNumberIsZero()
+	{
+		//Arrange
+		var intZero = (Int16)(Int16.MaxValue + Int16.MinValue + 1);
+
+		//Act
+		var actualWhenIntIsZero = intZero.IsZero();
+
+		//Assert
+		actualWhenIntIsZero.Should().BeTrue();
+	}
+}
